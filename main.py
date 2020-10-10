e83af740f04a251dba9188b48536568c7b77a5cc
@itsAj <ajaymati1000@gmail.com>
2020-10-10 22:31:39 +0530

    initialize

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 22:22:50 2020
+
+@author: Aju
+"""
+import matplotlib.pyplot as plt
+plt.plot([2,3,5,1,4,2])
+plt.show()
+
+plt.plot([2,4,5,2,12,1])
+plt.show()
