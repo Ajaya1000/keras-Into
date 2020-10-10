444850f9f52131fbda7da25eee2350dbd4715125
@itsAj <ajaymati1000@gmail.com>
2020-10-10 22:48:01 +0530

    added ga

---
+++ ga.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 22:37:45 2020
+
+@author: Aju
+"""
--- main.py
+++ main.py
@@ -4,9 +4,23 @@ Created on Sat Oct 10 22:22:50 2020
 
 @author: Aju
 """
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-plt.plot([2,3,5,1,4,2])
-plt.show()
+import tensorflow as tf
 
-plt.plot([2,4,5,2,12,1])
-plt.show()
+np.random.seed(42) 
+tf.random.set_seed(42)
+
+
+inputs = np.array([4,-2,3.5,5,-11,-4.7])
+
+n_weights = 6
+
+sol_per_pop = 8 #solution per population
+pop_size = (sol_per_pop,n_weights)
+
+#creating the initial population
+new_population = np.random.uniform(low=-4.0,high=4.0,size= pop_size)
+
+import ga
